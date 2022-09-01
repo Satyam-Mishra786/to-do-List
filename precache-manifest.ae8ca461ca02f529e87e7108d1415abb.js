38a14b6e06d28e97044c83c6c7ba76056f6ec08c
Satyam-Mishra786 <105660084+Satyam-Mishra786@users.noreply.github.com>
2022-09-01 07:26:43 -0700

    Updates

---
+++ precache-manifest.ae8ca461ca02f529e87e7108d1415abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae817aed206e8d93eb2",
+    "url": "/to-do-List/static/css/main.49c772d3.chunk.css"
+  },
+  {
+    "revision": "cae817aed206e8d93eb2",
+    "url": "/to-do-List/static/js/main.cae817ae.chunk.js"
+  },
+  {
+    "revision": "f281d8ac84cadebacf94",
+    "url": "/to-do-List/static/js/1.f281d8ac.chunk.js"
+  },
+  {
+    "revision": "278b61a1f2da3c3b38b8",
+    "url": "/to-do-List/static/js/runtime~main.278b61a1.js"
+  },
+  {
+    "revision": "1bf194b8a5d9887e158d62b831043a9f",
+    "url": "/to-do-List/index.html"
+  }
+];
